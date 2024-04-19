d235b4a39477988a5d0f38db33b46a8b14d0d9f8
Jasmine J <146461479+J-0987@users.noreply.github.com>
2024-04-19 11:29:47 -0400

    started routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,43 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    // be sure to include its associated Products
+    const data = await Category.findAll({
+      include: [Product]
+    });
+    res.json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const data = await Category.findByPk(req.params.id, {
+      include: [Product]
+    });
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const data = await Category.create(req.body);
+    res.json(data);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.put('/:id', (req, res) => {
